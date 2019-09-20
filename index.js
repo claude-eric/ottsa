66d57890ba7b840ac2c04ffcc6d6b017e62aa002
claude-eric <43693543+claude-eric@users.noreply.github.com>
2019-09-20 11:40:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var http = require('http');
+var ESpeak = require('node-espeak');
 
 var server = http.createServer(function(req, res) {
+  
+  ESpeak.initialize();
+  ESpeak.onVoice(function(wav, samples, samplerate) {
+      // TODO: Do something useful
+  });
+  ESpeak.speak("Hello world!");
+  
   res.writeHead(200);
   res.end('Salut tout le monde !');
-  
-  
-  var ESpeak = require('node-espeak');
-ESpeak.initialize();
-ESpeak.onVoice(function(wav, samples, samplerate) {
-    // TODO: Do something useful
-});
-ESpeak.speak("Hello world!");
 
 });
 server.listen(8080);
